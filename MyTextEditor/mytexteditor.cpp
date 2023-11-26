c03e77c55adbf8077c16f08b45b14f11bc758796
colin <1246575578@qq.com>
2023-11-26 23:36:17 +0800

    添加文本编辑功能

--- MyTextEditor/mytexteditor.cpp
+++ MyTextEditor/mytexteditor.cpp
@@ -8,6 +8,46 @@ CMyTextEditor::CMyTextEditor(QWidget *parent)
     ui->setupUi(this);
     setAcceptDrops(true);
 
+    this->fontDialog=new QFontDialog;
+    this->colorDialog_font=new QColorDialog;
+    this->colorDialog_fontBg=new QColorDialog;
+
+    //创建一个字体json文件
+    QFile file(strFileJson);
+
+    if (!file.exists())
+    {
+        // 文件不存在，创建新的空文件
+        if (!file.open(QIODevice::WriteOnly))
+        {
+            qDebug() << "无法创建新文件：" << strFileJson;
+            return;
+        }
+        file.close();
+    }
+
+    //设置上下文菜单策略
+    setContextMenuPolicy(Qt::DefaultContextMenu);//使用Qt::ActionsContextMenu的方式会失效
+
+    //查找框
+    this->DlgFind=new QDialog(this);
+    this->DlgFind->setWindowTitle("查找");
+    this->leFind=new QLineEdit(this->DlgFind);
+    this->lblFind=new QLabel(this->DlgFind);
+    this->lblFind->setText("");
+    QPushButton *btnBackward=new QPushButton("向后查找",this->DlgFind);
+    QPushButton *btnForward=new QPushButton("向前查找",this->DlgFind);
+    QVBoxLayout *vLayout=new QVBoxLayout(this->DlgFind);
+    vLayout->addWidget(this->leFind);
+    vLayout->addWidget(this->lblFind);
+
+    QHBoxLayout *hLayout=new QHBoxLayout(this->DlgFind);
+    hLayout->addWidget(btnBackward);
+    hLayout->addWidget(btnForward);
+    vLayout->addLayout(hLayout);
+
+    connect(btnBackward,&QPushButton::clicked,this,&CMyTextEditor::findTextBackward);
+    connect(btnForward,&QPushButton::clicked,this,&CMyTextEditor::findTextForward);
 
     //新建的页面
     for (int i = 0; i < MAXPAGE; ++i)
@@ -91,6 +131,7 @@ QWidget* CMyTextEditor::createTabPage(QString filename)
         tabPage->textEidt=td_new;
         tabPage->filePath=filename;
         m_mapWidget2TextEdit_Total.insert(tab_new,tabPage);
+
         return tab_new;
     }
 
@@ -204,12 +245,13 @@ void CMyTextEditor::dropEvent(QDropEvent *event)
 
             //新建一个
             QWidget *widget=createTabPage(fileName);
+            loadFileFont(widget);
             m_mapWidget2TextEdit_Total[widget]->textEidt->setText(in.readAll());
 
             int iIndex=ui->tabWidget->currentIndex();
             on_tabWidget_tabCloseRequested(iIndex);//这里必须先新增在删除，如果此时只有一个界面时，不先新增的话无法删除当前界面
 
-            ui->tabWidget->setCurrentWidget(widget);
+            ui->tabWidget->setCurrentWidget(widget); 
         }
     }
     else
@@ -238,27 +280,37 @@ void CMyTextEditor::on_tabWidget_tabCloseRequested(int index)
     //auto it_File=m_mapWidgt2TextEdit_File.find(tab_close);
     auto it=m_mapWidget2TextEdit_Total.find(tab_close);
 
-    if((*it)->bNew)//找到==是new n类型的界面
+    if((*it)->textEidt->document()->isModified())
     {
-        QString name=tab_close->objectName();
-        name=name.split("_").at(1);
-        bPageNew[std::stoi(name.toStdString())-1]=false;
-
-        m_mapWidget2TextEdit_Total.erase(it);
-
-    }
-    else
-    {
-        if((*it)->textEidt->document()->isModified())
+        if((*it)->bNew)//找到==是new n类型的界面
         {
             //另存为
+            QString fileName = QFileDialog::getSaveFileName(this,"另存为","D:");
+            saveAsFile(fileName);
+
+            QString name=tab_close->objectName();
+            name=name.split("_").at(1);
+            bPageNew[std::stoi(name.toStdString())-1]=false;
+
+            m_mapWidget2TextEdit_Total.erase(it);
+
         }
         else
         {
-            m_mapWidget2TextEdit_Total.erase(it);
+            bool bSave=saveFile((*it)->filePath);
+            if(bSave)
+            {
+                QMessageBox::warning(this,"提示",QString("文件保存到%1").arg((*it)->filePath));
+                saveFileFont(tab_close);
+                m_mapWidget2TextEdit_Total.erase(it);
+            }
         }
     }
-
+    else
+    {
+        //没改过就不做任何处理
+        qDebug()<<"没有修改文件";
+    }
 
     this->iPageCount--;
     delete tab_close;
@@ -278,6 +330,7 @@ void CMyTextEditor::on_action_Open_triggered()
         if(file.open(QFile::ReadOnly | QFile::Text))
         {
             QWidget *widget=createTabPage(file.fileName());
+            loadFileFont(widget);
 
             QTextStream in(&file); // 新建文本流对象
             QApplication::setOverrideCursor(Qt::WaitCursor);
@@ -286,6 +339,7 @@ void CMyTextEditor::on_action_Open_triggered()
             QApplication::restoreOverrideCursor();
             ui->tabWidget->setCurrentWidget(widget);
             file.close();
+
         }
         else
         {
@@ -338,5 +392,551 @@ void CMyTextEditor::on_actionl_SaveAs_triggered()
     QString fileName = QFileDialog::getSaveFileName(this,"另存为","D:");
     saveAsFile(fileName);
 
+}
+
+//复制
+void CMyTextEditor::on_action_Copy_triggered()
+{
+    QWidget *tab_save = ui->tabWidget->currentWidget();
+    m_mapWidget2TextEdit_Total[tab_save]->textEidt->copy();
+}
+
+//粘贴
+void CMyTextEditor::on_action_Paste_triggered()
+{
+    QWidget *tab_save = ui->tabWidget->currentWidget();
+    m_mapWidget2TextEdit_Total[tab_save]->textEidt->paste();
+}
+
+//撤销
+void CMyTextEditor::on_action_Undo_triggered()
+{
+    QWidget *tab_save = ui->tabWidget->currentWidget();
+    m_mapWidget2TextEdit_Total[tab_save]->textEidt->undo();
+}
+
+//剪切
+void CMyTextEditor::on_action_Cut_triggered()
+{
+    QWidget *tab_save = ui->tabWidget->currentWidget();
+    m_mapWidget2TextEdit_Total[tab_save]->textEidt->cut();
+}
+
+//查找
+void CMyTextEditor::on_action_Find_triggered()
+{
+    this->DlgFind->show();
+}
+
+//向后查找
+void CMyTextEditor::findTextBackward()
+{
+    QWidget *tab_save = ui->tabWidget->currentWidget();
+    QString str = this->leFind->text();
+
+    if(m_mapWidget2TextEdit_Total[tab_save]->textEidt->toPlainText().contains(str))
+    {
+        this->lblFind->setText("");
+        if (!m_mapWidget2TextEdit_Total[tab_save]->textEidt->find(str, QTextDocument::FindBackward))
+        {
+            this->lblFind->setText("到达文件头");
+            m_mapWidget2TextEdit_Total[tab_save]->textEidt->moveCursor(QTextCursor::End);
+        }
+    }
+    else
+    {
+        QMessageBox::warning(this->DlgFind,"查找",QString("找不到%1").arg(str));
+    }
+}
+
+//向前查找
+void CMyTextEditor::findTextForward()
+{
+    QWidget *tab_save = ui->tabWidget->currentWidget();
+    QString str = this->leFind->text();
+
+    if (m_mapWidget2TextEdit_Total[tab_save]->textEidt->toPlainText().contains(str))
+    {
+        this->lblFind->setText("");
+        if (!m_mapWidget2TextEdit_Total[tab_save]->textEidt->find(str))
+        {
+            this->lblFind->setText("到达文件尾");
+            m_mapWidget2TextEdit_Total[tab_save]->textEidt->moveCursor(QTextCursor::Start);
+        }
+    }
+    else
+    {
+        QMessageBox::warning(this->DlgFind, "查找", QString("找不到%1").arg(str));
+    }
+}
+
+//保存字体信息到文件
+//输入参数应该是完整的路径
+void CMyTextEditor::saveFileFont(QWidget *tab_cur)
+{
+    if(m_mapWidget2TextEdit_Total.find(tab_cur)==m_mapWidget2TextEdit_Total.end())
+    {
+        return;//没找到
+    }
+    QString fileName=m_mapWidget2TextEdit_Total[tab_cur]->filePath;
+
+    QTextCursor cursor=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->textCursor();
+    QFont font=cursor.charFormat().font();
+
+    QJsonObject fontAttributes;//json块
+    fontAttributes[FilePath] = fileName;
+    fontAttributes[FontFamily] = font.family();
+    fontAttributes[FontSize] = font.pointSize();
+    fontAttributes[FontWeight] = font.weight();
+    fontAttributes[Underline] = font.underline();
+    fontAttributes[Strikeout] = font.strikeOut();
+    fontAttributes[Italic] = font.italic();
+
+    QFile file(strFileJson);
+    QJsonDocument jsonDoc;
+
+    if(file.exists() && file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QTextStream in(&file);
+        QString content=in.readAll();
+        file.close();
+
+        jsonDoc=QJsonDocument::fromJson(content.toUtf8());
+    }
+    else
+    {
+        //不存在
+        qDebug()<<"找不到Json文件："<<strFileJson;
+        return;
+    }
+
+    QJsonArray jsonArray=jsonDoc.array();
+
+    bool bFindFilePath=false;//是否已经存在该文件字段
+    for (int i = 0; i < jsonArray.size(); ++i)
+    {
+        QJsonObject obj = jsonArray.at(i).toObject();
+        if (obj[FilePath].toString() == fontAttributes[FilePath].toString())
+        {
+            // 更新已存在的块
+            obj = fontAttributes;
+            jsonArray.replace(i, obj);
+            bFindFilePath = true;
+            break;
+        }
+    }
+
+    // 如果不存在，添加新块
+    if (!bFindFilePath)
+    {
+        jsonArray.append(fontAttributes);
+    }
+
+    jsonDoc.setArray(jsonArray);
+
+    // 保存 JSON 到文件
+    if (file.open(QIODevice::WriteOnly | QIODevice::Text))
+    {
+        QTextStream out(&file);
+        out << jsonDoc.toJson();
+        file.close();
+    }
+}
+
+//加载字体信息到
+void CMyTextEditor::loadFileFont(QWidget *widget)
+{
+    if(m_mapWidget2TextEdit_Total.find(widget)==m_mapWidget2TextEdit_Total.end())
+    {
+        return;//没找到
+    }
+    QString fileName=m_mapWidget2TextEdit_Total[widget]->filePath;
+
+    QFile file(strFileJson);
+    if (!file.exists() || !file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        // 文件不存在或无法打开
+        return;
+    }
+
+    QTextStream in(&file);
+    QString content = in.readAll();
+    file.close();
+
+    QJsonDocument jsonDoc = QJsonDocument::fromJson(content.toUtf8());
+
+    // 获取 JSON 数组
+    QJsonArray jsonArray = jsonDoc.array();
+
+    // 查找是否存在相同的 filePath
+    for (int i = 0; i < jsonArray.size(); ++i)
+    {
+        QJsonObject obj = jsonArray.at(i).toObject();
+        if (obj[FilePath].toString() == fileName)
+        {
+            // 使用 JSON 中的字体信息更新当前 widget
+            setFontFamily(obj[FontFamily].toString(), widget,Opt_All);
+            setFontSize(obj[FontSize].toInt(), widget,Opt_All);
+            setFontWeight(obj[FontWeight].toInt(), widget,Opt_All);
+            setUnderline(obj[Underline].toBool(), widget,Opt_All);
+            setStrikeout(obj[Strikeout].toBool(), widget,Opt_All);
+            setItalic(obj[Italic].toBool(), widget,Opt_All);
+            break;
+        }
+    }
+}
+
+//设置字体类型
+void CMyTextEditor::setFontFamily(QString strFontFamily,QWidget *widget,int iType)
+{
+    QWidget *tab_cur=nullptr;
+    if(widget==nullptr)
+    {
+        tab_cur = ui->tabWidget->currentWidget();
+    }
+    else
+    {
+        tab_cur = widget;
+    }
+
+    if(iType==Opt_Selected)//设置选中的文本
+    {
+        QTextCursor cursor=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->textCursor();
+        QTextCharFormat format = cursor.charFormat();
+
+        // 设置字体
+        format.setFont(strFontFamily);
+
+        // 应用字体属性到当前选中的文本
+        cursor.mergeCharFormat(format);
+    }
+    else
+    {
+        QTextDocument *document=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->document();
+
+        QTextCharFormat format;
+        format.setFont(strFontFamily);
+        document->setDefaultFont(format.font());
+    }
+
+}
+
+//设置字体大小
+void CMyTextEditor::setFontSize(int iFontSize,QWidget *widget,int iType)
+{
+    QWidget *tab_cur=nullptr;
+    if(widget==nullptr)
+    {
+        tab_cur = ui->tabWidget->currentWidget();
+    }
+    else
+    {
+        tab_cur = widget;
+    }
+
+    if(iType==Opt_Selected)//设置选中的文本
+    {
+        QTextCursor cursor=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->textCursor();
+        QTextCharFormat format = cursor.charFormat();
+
+        // 设置字体大小
+        format.setFontPointSize(iFontSize);
+
+
+        // 应用字体属性到当前选中的文本
+        cursor.mergeCharFormat(format);
+    }
+    else
+    {
+        QTextDocument *document=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->document();
+
+        QTextCharFormat format;
+        format.setFontPointSize(iFontSize);
+        document->setDefaultFont(format.font());
+    }
+}
+
+
+//设置字体粗细
+void CMyTextEditor::setFontWeight(int iWeight,QWidget *widget,int iType)
+{
+    QWidget *tab_cur=nullptr;
+    if(widget==nullptr)
+    {
+        tab_cur = ui->tabWidget->currentWidget();
+    }
+    else
+    {
+        tab_cur = widget;
+    }
+
+    if(iType==Opt_Selected)//设置选中的文本
+    {
+        QTextCursor cursor=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->textCursor();
+        QTextCharFormat format = cursor.charFormat();
+
+        //设置字体粗细
+        format.setFontWeight(iWeight);
+
+        // 应用字体属性到当前选中的文本
+        cursor.mergeCharFormat(format);
+    }
+    else//设置全体文本
+    {
+        QTextDocument *document=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->document();
+
+        QFont defaultFont = document->defaultFont();
+        QFont::Weight weight = static_cast<QFont::Weight>(iWeight);
+        defaultFont.setWeight(weight);
+
+        QTextCharFormat format;
+        format.setFont(defaultFont);
+
+        document->setDefaultFont(defaultFont);
+
+
+    }
+}
+
+//设置下划线
+void CMyTextEditor::setUnderline(bool bUnderline,QWidget *widget,int iType)
+{
+    QWidget *tab_cur=nullptr;
+    if(widget==nullptr)
+    {
+        tab_cur = ui->tabWidget->currentWidget();
+    }
+    else
+    {
+        tab_cur = widget;
+    }
+
+    if(iType==Opt_Selected)//设置选中的文本
+    {
+        QTextCursor cursor=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->textCursor();
+        QTextCharFormat format = cursor.charFormat();
+
+        //设置下划线
+        format.setFontUnderline(bUnderline);
+
+        // 应用字体属性到当前选中的文本
+        cursor.mergeCharFormat(format);
+    }
+    else//设置整个文本
+    {
+        QTextDocument *document=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->document();
+
+        QFont defaultFont = document->defaultFont();
+        defaultFont.setUnderline(bUnderline);
+
+        QTextCharFormat format;
+        format.setFont(defaultFont);
+
+        document->setDefaultFont(defaultFont);
+
+    }
+}
+
+//设置删除线
+void CMyTextEditor::setStrikeout(bool bStrikeout,QWidget *widget,int iType)
+{
+    QWidget *tab_cur=nullptr;
+    if(widget==nullptr)
+    {
+        tab_cur = ui->tabWidget->currentWidget();
+    }
+    else
+    {
+        tab_cur = widget;
+    }
+
+    if(iType==Opt_Selected)//设置选中的文本
+    {
+        QTextCursor cursor=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->textCursor();
+        QTextCharFormat format = cursor.charFormat();
+
+        //设置删除线
+        format.setFontStrikeOut(bStrikeout);
+
+        // 应用字体属性到当前选中的文本
+        cursor.mergeCharFormat(format);
+    }
+    else
+    {
+        QTextDocument *document=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->document();
+
+        QFont defaultFont = document->defaultFont();
+        defaultFont.setStrikeOut(bStrikeout);
+
+        QTextCharFormat format;
+        format.setFont(defaultFont);
+
+        document->setDefaultFont(defaultFont);
+    }
+}
+
+//字体倾斜
+void CMyTextEditor::setItalic(bool bItalic, QWidget *widget,int iType)
+{
+    QWidget *tab_cur=nullptr;
+    if(widget==nullptr)
+    {
+        tab_cur = ui->tabWidget->currentWidget();
+    }
+    else
+    {
+        tab_cur = widget;
+    }
+
+    if(iType==Opt_Selected)//设置选中的文本
+    {
+        QTextCursor cursor=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->textCursor();
+        QTextCharFormat format = cursor.charFormat();
+
+        //设置删除线
+        format.setFontItalic(bItalic);
+
+        // 应用字体属性到当前选中的文本
+        cursor.mergeCharFormat(format);
+    }
+    else
+    {
+        QTextDocument *document=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->document();
+
+        QFont defaultFont = document->defaultFont();
+        defaultFont.setItalic(bItalic);
+
+        QTextCharFormat format;
+        format.setFont(defaultFont);
+
+        document->setDefaultFont(defaultFont);
+
+    }
+
+}
+
+//字体加粗
+void CMyTextEditor::on_action_TextBold_triggered()
+{
+    QWidget *tab_cur = ui->tabWidget->currentWidget();
+    QTextCursor cursor=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->textCursor();
+    QTextCharFormat format = cursor.charFormat();
+
+    format.setFontWeight((format.fontWeight() == QFont::Black) ? QFont::Normal : QFont::Black);
+
+
+    // 应用字体属性到当前选中的文本
+    cursor.mergeCharFormat(format);
+}
+
+//字体
+void CMyTextEditor::on_action_TextType_triggered()
+{
+    //先将当前字体信息设置进去
+    QWidget *tab_cur = ui->tabWidget->currentWidget();
+    QTextCursor cursor=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->textCursor();
+    QFont font=cursor.charFormat().font();
+    this->fontDialog->setCurrentFont(font);
+
+    if (this->fontDialog->exec() == QDialog::Accepted)
+    {
+        QFont selectedFont = this->fontDialog->selectedFont();
+        //qDebug()<<selectedFont;
+
+        QString fontFamily = selectedFont.family();  // 获取字体类型
+        setFontFamily(fontFamily);
+        int fontSize = selectedFont.pointSize();     // 获取字体大小
+        setFontSize(fontSize);
+        int fontWeight = selectedFont.weight();      // 获取字体粗细
+        setFontWeight(fontWeight);
+        bool underline = selectedFont.underline();   // 获取是否有下划线
+        setUnderline(underline);
+        bool strikeout = selectedFont.strikeOut();   // 获取是否有删除线
+        setStrikeout(strikeout);
+
+    }
+
+}
+
+//字体倾斜
+void CMyTextEditor::on_action_TextItalic_triggered()
+{
+    QWidget *tab_cur = ui->tabWidget->currentWidget();
+    QTextCursor cursor=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->textCursor();
+    QTextCharFormat format = cursor.charFormat();
+
+    format.setFontItalic(!format.fontItalic());
+
+    // 应用字体属性到当前选中的文本
+    cursor.mergeCharFormat(format);
+}
+
+//删除线
+void CMyTextEditor::on_action_TextStrike_triggered()
+{
+    QWidget *tab_cur = ui->tabWidget->currentWidget();
+    QTextCursor cursor=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->textCursor();
+    QTextCharFormat format = cursor.charFormat();
+
+    format.setFontStrikeOut(!format.fontStrikeOut());
+
+    // 应用字体属性到当前选中的文本
+    cursor.mergeCharFormat(format);
+}
+
+//下划线
+void CMyTextEditor::on_action_TextUnder_triggered()
+{
+    QWidget *tab_cur = ui->tabWidget->currentWidget();
+    QTextCursor cursor=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->textCursor();
+    QTextCharFormat format = cursor.charFormat();
+
+    format.setFontUnderline(!format.fontUnderline());
+
+    // 应用字体属性到当前选中的文本
+    cursor.mergeCharFormat(format);
+}
+
+//关于
+void CMyTextEditor::on_action_About_triggered()
+{
+    QWidget *tab_cur = ui->tabWidget->currentWidget();
+    loadFileFont(tab_cur);
+}
+
+//字体颜色
+void CMyTextEditor::on_action_color_triggered()
+{
+    QColor color= this->colorDialog_font->getColor(Qt::black,this,"字体颜色");
+
+    if(color.isValid())
+    {
+        QWidget *tab_cur = ui->tabWidget->currentWidget();
+        QTextCursor cursor=m_mapWidget2TextEdit_Total[tab_cur]->textEidt->textCursor();
+        QTextCharFormat format = cursor.charFormat();
+
+        format.setForeground(color);
+
+        // 应用字体属性到当前选中的文本
+        cursor.mergeCharFormat(format);
+
+    }
+}
+
+//字体背景颜色
+void CMyTextEditor::on_action_bgColor_triggered()
+{
+    QColor color = this->colorDialog_fontBg->getColor(Qt::white, this, "字体背景颜色");
+
+    if (color.isValid())
+    {
+        QWidget *tab_cur = ui->tabWidget->currentWidget();
+        QTextCursor cursor = m_mapWidget2TextEdit_Total[tab_cur]->textEidt->textCursor();
+        QTextCharFormat format = cursor.charFormat();
+
+        // 设置字体背景颜色
+        format.setBackground(QBrush(color));
+        cursor.setCharFormat(format);  // 设置新的背景颜色
+    }
 }
 
--- MyTextEditor/mytexteditor.h
+++ MyTextEditor/mytexteditor.h
@@ -10,9 +10,32 @@
 #include <QString>
 #include <QDragEnterEvent>
 #include <QMimeData>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
+#include <QAction>
+#include <QFontDialog>
+#include <QColorDialog>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+
+#define FilePath    "filePath"
+#define FontFamily  "fontFamily"
+#define FontSize    "fontSize"
+#define FontWeight  "fontWeight"
+#define Underline   "underline"
+#define Strikeout   "strikeout"
+#define Italic      "italic"
 
 //最大新建页面数(new n)
 #define MAXPAGE 5
+
+enum OptFontType
+{
+    Opt_Selected=0,
+    Opt_All,
+};
 
 struct strTabPage //一个tabPage存在的东西
 {
@@ -51,6 +74,21 @@ protected:
     void dropEvent(QDropEvent * event);
 
 private slots:
+    void findTextBackward();
+    void findTextForward();
+
+    void saveFileFont(QWidget *widget);
+    void loadFileFont(QWidget *widget);
+
+    void setFontFamily(QString strFontFamily,QWidget *widget=nullptr,int iType=Opt_Selected);
+    void setFontSize(int iFontSize,QWidget *widget=nullptr,int iType=Opt_Selected);
+    void setFontWeight(int iWeight,QWidget *widget=nullptr,int iType=Opt_Selected);
+    void setUnderline(bool bUnderline=true,QWidget *widget=nullptr,int iType=Opt_Selected);
+    void setStrikeout(bool bStrikeout=true,QWidget *widget=nullptr,int iType=Opt_Selected);
+    void setItalic(bool bItalic=true,QWidget *widget=nullptr,int iType=Opt_Selected);
+
+
+private slots:
     void on_action_New_triggered();
 
     void on_tabWidget_tabCloseRequested(int index);
@@ -61,6 +99,32 @@ private slots:
 
     void on_actionl_SaveAs_triggered();
 
+    void on_action_Copy_triggered();
+
+    void on_action_Paste_triggered();
+
+    void on_action_Undo_triggered();
+
+    void on_action_Cut_triggered();
+
+    void on_action_Find_triggered();
+
+    void on_action_TextBold_triggered();
+
+    void on_action_TextType_triggered();
+
+    void on_action_TextItalic_triggered();
+
+    void on_action_TextStrike_triggered();
+
+    void on_action_TextUnder_triggered();
+
+    void on_action_About_triggered();
+
+    void on_action_color_triggered();
+
+    void on_action_bgColor_triggered();
+
 private:
     Ui::CMyTextEditor *ui;
 
@@ -70,5 +134,14 @@ private:
     //QMap<QWidget*,QTextEdit*> m_mapWidgt2TextEdit_New;//一一对应,新增的页面
     //QMap<QWidget*,QTextEdit*> m_mapWidgt2TextEdit_File;//一一对应,打开文件的页面
     QMap<QWidget*,strTabPage*> m_mapWidget2TextEdit_Total;//合二为一，通过bNew来判断
+
+    QDialog *DlgFind;
+    QLineEdit *leFind;
+    QLabel *lblFind;
+
+    QFontDialog *fontDialog;
+    const QString strFileJson="./fontType.json";
+    QColorDialog *colorDialog_font;
+    QColorDialog *colorDialog_fontBg;
 };
 #endif // CMYTEXTEDITOR_H
